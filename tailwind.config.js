fa24ffb1554dea2db872c3f0082c9226e6c48503
SolihovAbdulloh <abdullasolikh@mail.com>
2024-08-31 02:27:14 +0500

    header qismi

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,25 @@
 module.exports = {
   content: ["./pages/*.{html,js}"],
   theme: {
+    colors: {
+      oq: "#ffff",
+      kok: "#1fb6ff",
+      purple: "#7e5bef",
+      pushti: "#ff49db",
+      olovrang: "#ff7849",
+      yashil: "#13ce66",
+      yellow: "#ffc82c",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+      green: "#5EB083",
+      bejiviy: "#FFBB8A",
+      kokimtir: "#E5EDFA",
+      ochyashil: "#EDFFF5",
+    },
+    fontFamily: {
+      sans: ["Roboto"],
+    },
     extend: {},
   },
   plugins: [],
